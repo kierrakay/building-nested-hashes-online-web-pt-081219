e5cf6fcf202ca59bdde07872cc78111a63b0d236
Kierra M <k.merriman32@hotmail.com>
2019-08-29 19:56:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,14 @@ def second_challenge
       :hero_friends => {}
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+     :montague => {
+     :patriarch => {},
+     :matriarch => {},
+     :hero => {},
+      :hero_friends => {}
+      }
+    
+  }
 
   
 end
