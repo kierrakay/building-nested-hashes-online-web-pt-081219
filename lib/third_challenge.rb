74d85a3944f5180063bf4234a527ebbcbcba8c4f
Kierra M <k.merriman32@hotmail.com>
2019-08-29 20:10:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,15 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: {}, age: {}},
-      :matriarch => {name: {}, age:{}},
-      :hero => {name: {}, age:{}},
+      :patriarch => {name: {}, age: {}, status:{}},
+      :matriarch => {name: {}, age:{}, status:{}},
+      :hero => {name: {}, age:{}, status:{}},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {name:{}, age:{}},
-      :matriarch => {name:{}, age:{}},
-      :heroine => {name:{}, age:{}},
+      :patriarch => {name:{}, age:{}, status:{}},
+      :matriarch => {name:{}, age:{},status:{}},
+      :heroine => {name:{}, age:{}},status:{},
       :heroine_friends => []
    }
   }
