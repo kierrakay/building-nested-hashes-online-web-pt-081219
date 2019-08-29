21f3652c15235195d794a6fa4b8e16f49f273967
Kierra M <k.merriman32@hotmail.com>
2019-08-29 19:59:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -14,24 +14,6 @@ def second_challenge
       }
     }
   
-
-
- epic_tragedy = {
-    :montague => {
-     :patriarch => {},
-     :matriarch => {},
-     :hero => {},
-     :hero_friends => []
-    :capulet => {
-     :patriarch => {},
-     :matriarch => {},
-     :heroine => {},
-     :heroine_friends => []
-      }
-  }
-
-  
 end
-  
-end
-      }, 
+
+ 
